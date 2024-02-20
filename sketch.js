72ba784f4ed529d506ba79f4bfc895b9dffa669b
lakshmiJL <141428624+lakshmiJL@users.noreply.github.com>
2024-02-21 00:08:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//Part 1
+var obs1, obs2,obs3, obs4,obs5,edges;
+var pirate;
+function setup() {
+  createCanvas(400, 400);
+ 
+  
+}
+
+
+function draw() {
+  background("skyblue");
+  fill("white")
+    drawSprites();
+}
